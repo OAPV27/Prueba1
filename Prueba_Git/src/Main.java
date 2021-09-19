b78ad34adc452d2b5ba02b0e6ae277bae9a22cab
Ian Williams <iancrpb@hotmail.com>
2021-09-19 14:01:55 -0600

    Cambio a JOption

--- Prueba_Git/src/Main.java
+++ Prueba_Git/src/Main.java
@@ -1,10 +1,10 @@
-
+import javax.swing.*;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			
-		System.out.println("Hola mundo 2");
+		JOptionPane.showMessageDialog(null, "hola");
 	}
 
 }

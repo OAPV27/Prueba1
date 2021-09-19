f1d7414a94bdd6630f2e1dec4972f9213cf2dbb7
Patrik Williams <iancrpb@hotmail.com>
2021-09-18 20:52:10 -0600

    Primer commit

--- Hola mundo/src/hola/mundo/HolaMundo.java
+++ Hola mundo/src/hola/mundo/HolaMundo.java
@@ -17,7 +17,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("hola.mundo");
+        System.out.println("hola.mundo 1 ");
     }
     
 }
